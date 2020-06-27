f5c8915c86e539a249720a63dd7e45d4ee38c57d
Daniele Chiappalupi <dchiappal@student.ethz.ch>
2020-06-27 13:15:18 +0200

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cil-road-extraction-project',
+    version='1.0',
+    packages=find_packages(exclude=[]),
+    url='https://github.com/daniCh8/road-segmentation-eth-cil-2020',
+
+    author='danich',
+
+    python_requires='>=3.5',
+    install_requires=[
+            # Add external libraries here.
+            'tensorflow-gpu==1.12.0',
+            'scikit-image',
+            'albumentations',
+            'tqdm',
+            'keras',
+            'cv2',
+            'numpy',
+            'matplotlib'
+    ]
+)
