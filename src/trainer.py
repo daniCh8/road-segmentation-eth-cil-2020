772ba2315e6be1e21451de9e6b954d6a7a091573
Daniele Chiappalupi <dchiappal@student.ethz.ch>
2020-07-19 13:54:07 +0200

    fixed errors after update

--- src/trainer.py
+++ src/trainer.py
@@ -10,30 +10,30 @@ from utils import single_model_training, save_predictions_pdf
 
 
 def main_train(config):
-    for net_to_train in config['net_types']:
+    for net_to_train, net_name in zip(config['net_types'], config['net_names']):
         net = NNet(net_type=net_to_train, data_paths=config['data_paths'])
         single_model_training(model=net,
-                              save_path=config[net_to_train]['checkpoint'],
-                              additional_epochs=config[net_to_train]['additional_epochs'],
-                              competition_epochs=config[net_to_train]['competition_epochs'],
-                              b_size=config[net_to_train]['batch_size'],
+                              save_path=config[net_name]['checkpoint'],
+                              additional_epochs=config[net_name]['additional_epochs'],
+                              competition_epochs=config[net_name]['competition_epochs'],
+                              b_size=config[net_name]['batch_size'],
                               loss=config['loss'],
                               l_rate_a=config['learning_rate_additional_data'],
                               l_rate_b=config['learning_rate_competition_data'],
                               v=config['verbose'])
 
         predictions = net.predict_test_data()
-        np.save(file=config[net_to_train]['predictions_path'], arr=predictions)
-        print('saved model predictions at path: {}'.format(config[net_to_train]['predictions_path']))
-        sub = net.create_submission_file(path=config[net_to_train]['csv_path'], treshold=config['treshold'])
-        print('saved model csv at path: {}'.format(config[net_to_train]['csv_path']))
+        np.save(file=config[net_name]['predictions_path'], arr=predictions)
+        print('saved model predictions at path: {}'.format(config[net_name]['predictions_path']))
+        sub = net.create_submission_file(path=config[net_name]['csv_path'], treshold=config['treshold'])
+        print('saved model csv at path: {}'.format(config[net_name]['csv_path']))
 
 
 def main_predict(config):
     predictions = []
 
-    for net_type in config['net_types']:
-        preds = np.load(config[net_type]['predictions_path'], allow_pickle=True)
+    for net_name in config['net_names']:
+        preds = np.load(config[net_name]['predictions_path'], allow_pickle=True)
         predictions.append(preds)
     mean_ensemble = np.mean(np.array(predictions), axis=0)
     np.save(file=config['final_predictions_path'], arr=mean_ensemble)
@@ -48,7 +48,7 @@ def main_predict(config):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--save_path', dest='save_path', type=str, default='default', help='name of dir where to store training outputs')
+    parser.add_argument('-p', '--save_path', dest='save_path', type=str, default='', help='name of dir where to store training outputs')
     parser.add_argument('-n', '--nets_to_train', dest='net_names', action='append', default=[], help='list of names of the networks to train')
     parser.add_argument('-s', '--scope', dest='scope', type=str, default='train', help='scope of the job - can be either train or predict')
     parser.add_argument('-a', '--add_epochs', dest='additional_epochs', type=int, action='append', default=[], help='list of number of epochs used to train the nets on the google data')
